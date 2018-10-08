65a45f35e148884f5a89ceb0de134c994707f37a
Mario Andres Ravina <ayarmarxio@gmail.com>
2018-10-09 01:47:59 +0200

    first

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,34 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+let btnToOunces: HTMLButtonElement = <HTMLButtonElement>document.getElementById("conv-2-ounces-btn");
+btnToOunces.addEventListener("click", ConvToOuncesBtn);
+
+let btnToGrams: HTMLButtonElement = <HTMLButtonElement>document.getElementById("conv-2-grams-btn");
+btnToGrams.addEventListener("click", ConvToGramsBtn);
+
+function ConvToOuncesBtn(){
+    let weightInput: HTMLInputElement = <HTMLInputElement>document.getElementById("input-value");
+    let weightNumber = convertInput(weightInput);
+    let result = weightNumber * 0.035274;
+    displayResult(result); 
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function ConvToGramsBtn(){
+    let weightInput: HTMLInputElement = <HTMLInputElement>document.getElementById("input-value");
+    let weightNumber = convertInput(weightInput);
+    let result = weightNumber * 28.35;
+    displayResult(result); 
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+function convertInput(weightInput:HTMLInputElement){
+    let inputValue: string = weightInput.value;  
+    let weightNumber: number = +inputValue;
+    return weightNumber;
+}
+
+
+function displayResult(result:number){
+    let stringResult: string = String(result); 
+
+    let displayElement: HTMLSpanElement = <HTMLSpanElement> document.getElementById("result");
+    displayElement.innerHTML = stringResult;
+}
+
